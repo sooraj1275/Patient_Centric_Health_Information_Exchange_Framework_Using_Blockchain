68762a7ce7da3122ecfa9225a9fba938e2196ae7
sooraj1275 <84695729+sooraj1275@users.noreply.github.com>
2022-04-03 17:49:40 +0530

    Third commit

--- DBConnection.py
+++ DBConnection.py
@@ -1,14 +1,15 @@
 import mysql.connector
 
+
 class Db:
+    def __init__(self):
+        self.cnx = mysql.connector.connect(host="localhost",user="root",password="",database="textsummarization")
 
-    def __init__(self) -> object:
-
-        self.cnx = mysql.connector.connect(host="localhost", user="root", password="", database="patient_centric")
         self.cur = self.cnx.cursor(dictionary=True)
 
 
     def select(self, q):
+        print()
         self.cur.execute(q)
         return self.cur.fetchall()
 
@@ -18,17 +19,20 @@ class Db:
 
 
     def insert(self, q):
+
         self.cur.execute(q)
         self.cnx.commit()
         return self.cur.lastrowid
 
     def update(self, q):
-        self.cur.execute(q)
+        print()
+        a=self.cur.execute(q)
         self.cnx.commit()
-        return self.cur.rowcount
+        return a
 
     def delete(self, q):
         self.cur.execute(q)
         self.cnx.commit()
         return self.cur.rowcount
+
 
